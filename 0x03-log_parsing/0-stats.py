c7d6e82bc1a98249ed3bef2239a82a4c1d87dcb0
Mahiuha <tiasho.mahiuha@gmail.com>
2022-04-28 21:14:37 +0300

    reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,54 @@
 #!/usr/bin/python3
-"""Log parsing """
 
-if __name__ == '__main__':
-    import sys
-    import collections
+import sys
 
-    size, count = 0, 1
 
-    my_dict = {"500": 0, "301": 0, "400": 0, "401": 0,
-               "403": 0, "404": 0, "405": 0, "200": 0}
+def print_msg(dict_sc, total_file_size):
+    """
+    Method to print
+    Args:
+        dict_sc: dict of status codes
+        total_file_size: total of the file
+    Returns:
+        Nothing
+    """
 
-    def print_stadistics(size, my_dict):
-        """print metrics"""
-        print("File size: {}".format(size))
-        od = collections.OrderedDict(sorted(my_dict.items()))
-        for status, variable in od.items():
-            if variable != 0:
-                print("{}: {}".format(status, variable))
+    print("File size: {}".format(total_file_size))
+    for key, val in sorted(dict_sc.items()):
+        if val != 0:
+            print("{}: {}".format(key, val))
 
-    try:
-        """Validation computes metrics and print stadistics"""
-        for line in sys.stdin:
-            word = line.split()
-            try:
-                size += int(word[-1])
-                for status in my_dict.keys():
-                    if (word[-2] == status):
-                        my_dict[status] += 1
-                if count == 10:
-                    print_stadistics(size, my_dict)
-                    count = 0
-            except:
-                pass
-            count += 1
-    except KeyboardInterrupt:
-        """Keyboard interrupt"""
-        print_stadistics(size, my_dict)
-        exit(0)
-    print_stadistics(size, my_dict)
+
+total_file_size = 0
+code = 0
+counter = 0
+dict_sc = {"200": 0,
+           "301": 0,
+           "400": 0,
+           "401": 0,
+           "403": 0,
+           "404": 0,
+           "405": 0,
+           "500": 0}
+
+try:
+    for line in sys.stdin:
+        parsed_line = line.split()  # ✄ trimming
+        parsed_line = parsed_line[::-1]  # inverting
+
+        if len(parsed_line) > 2:
+            counter += 1
+
+            if counter <= 10:
+                total_file_size += int(parsed_line[0])  # file size
+                code = parsed_line[1]  # status code
+
+                if (code in dict_sc.keys()):
+                    dict_sc[code] += 1
+
+            if (counter == 10):
+                print_msg(dict_sc, total_file_size)
+                counter = 0
+
+finally:
+    print_msg(dict_sc, total_file_size)
