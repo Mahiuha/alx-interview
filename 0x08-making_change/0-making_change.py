96c7044274e8eca5c3b952e25fd40c35051bb443
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-01 20:21:23 +0300

    Making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,36 +1,25 @@
 #!/usr/bin/python3
-"""
-importing modules
-"""
-from math import *
-from cs50 import *
-
-
-def main():
-    # prompting user for change owed
-    while True:
-        coins = float(input("Change owed: "))
-        total = floor(coins * 100)
-        # checks if user input is greater than zero otherwise repeats step one
-        if total > 0:
-            return 0
-            break
-
-    makeChange(coins, total)
+""" MakeChange module"""
 
 
 def makeChange(coins, total):
-    # calculates quaters
-    quarter = total // 25
-    # calculates dimes
-    dime = (total % 25) // 10
-    # calculates nickels
-    nickel = ((total % 25) % 10) // 5
-    # calculates pennies
-    pennie = ((total % 25) % 10) % 5
-    # prints the change owed
-    print(f"{quarter + dime + nickel + pennie}")
+    """
+    find the minimum number of coins that meets the total number
+    """
+    if total <= 0:
+        return 0
 
+    if not coins or min(coins) > total:
+        return -1
 
-if __name__ == "__main__":
-    main()
+    INT_MAX = 1 << 32
+    totalSize = [INT_MAX] * (total + 1)
+
+    totalSize[0] = 0
+
+    for i in range(1, total + 1):
+        for coin in coins:
+            if coin <= i:
+                totalSize[i] = min((totalSize[i-coin] + 1), totalSize[i])
+
+    return totalSize[total] if totalSize[total] != INT_MAX else -1
