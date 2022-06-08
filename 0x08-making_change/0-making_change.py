83ce1331b9de34d75bc89e3bb4a755e4a1017368
Mahiuha <tiasho.mahiuha@gmail.com>
2022-06-09 01:01:30 +0300

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,23 +1,24 @@
 #!/usr/bin/python3
-"""Making Change problem
-"""
+
+""" Contains makeChange function"""
 
 
 def makeChange(coins, total):
-    """Returns the fewest number of coins needed to meet a given total
-    Args:
-        coins: array of integers representing the coin values
-        total: the total to find coins for
     """
+    Returns: fewest number of coins needed to meet total
+        If total is 0 or less, return 0
+        If total cannot be met by any number of coins you have, return -1
+    """
+    if not coins or coins is None:
+        return -1
     if total <= 0:
         return 0
-
-    array = [float('inf')] * (total + 1)
-    array[0] = 0
-
-    for i in range(1, len(array)):
-        for j in range(len(coins)):
-            if coins[j] <= i:
-                array[i] = min(array[i], array[i - coins[j]] + 1)
-
-    return array[i] if array[i] != float('inf') else -1
+    change = 0
+    coins = sorted(coins)[::-1]
+    for coin in coins:
+        while coin <= total:
+            total -= coin
+            change += 1
+        if (total == 0):
+            return change
+    return -1
